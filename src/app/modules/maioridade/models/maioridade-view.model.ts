1a88ba23e429be734578114681f98f2f9f593fec
Loran Henrique Teixeira <loran@ciandt.com>
2022-02-16 17:15:40 -0300

    criando layout da parte de maioridade

---
+++ src/app/modules/maioridade/models/maioridade-view.model.ts
@@ -0,0 +1,7 @@
+export interface MaioridadeViewModel {
+  titulo: string;
+  subtitulo: string;
+  textoBotaoPrimary: string;
+  textoBotaoSecondary: string;
+  tipoBotaoSecondary: string;
+}
--- src/app/modules/maioridade/pages/maioridade-page.component.ts
+++ src/app/modules/maioridade/pages/maioridade-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { TipoBotaoConstantes } from '@config/tipo-botao.const';
+import { MaioridadeViewModel } from '@maioridade/models/maioridade-view.model';
 
 @Component({
   selector: 'app-maioridade-page',
   templateUrl: './maioridade-page.component.html',
-  styleUrls: ['./maioridade-page.component.scss']
+  styleUrls: ['./maioridade-page.component.scss'],
 })
 export class MaioridadePageComponent implements OnInit {
+  public viewModel: MaioridadeViewModel;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.construirViewModel();
   }
 
+  private construirViewModel(): void {
+    this.viewModel = {
+      titulo: 'MAIORIDADE__TITULO',
+      subtitulo: 'MAIORIDADE__SUBTITULO',
+      textoBotaoPrimary: 'MAIORIDADE__BOTAO--SIM',
+      textoBotaoSecondary: 'MAIORIDADE__BOTAO--NAO',
+      tipoBotaoSecondary: TipoBotaoConstantes.SECONDARY,
+    };
+  }
 }
