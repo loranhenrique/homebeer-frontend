ccd7e386dac2eb753145de7c9b0799ba1459f63c
Loran Henrique Teixeira <loran@ciandt.com>
2022-02-16 00:14:56 -0300

    ajustando erros apontados pelo lint

--- src/app/modules/onboarding/onboarding-routing.module.ts
+++ src/app/modules/onboarding/onboarding-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { OnboardingPageComponent } from './pages/onboarding-page.component';
 
--- src/app/modules/onboarding/pages/onboarding-page.component.ts
+++ src/app/modules/onboarding/pages/onboarding-page.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-onboarding-page',
+  selector: 'bra-onboarding-page',
   templateUrl: './onboarding-page.component.html',
-  styleUrls: ['./onboarding-page.component.scss']
+  styleUrls: ['./onboarding-page.component.scss'],
 })
 export class OnboardingPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
